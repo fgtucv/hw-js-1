ad3ce6a56225a9939413771b282a10b06e6cd000
fgtucv <oles.slava23@gmail.com>
2023-07-31 12:09:20 +0300

    F

---
+++ HW-3/js/script2.js
@@ -0,0 +1,23 @@
+let c1 = "5px";
+
+let c2 = "12djd334";
+
+let c3 = "12.45asdwe12";
+
+let c4 = "qwqweeewq";
+
+c1 = Number(c1)
+
+c2 = Number(c2)
+
+c3 = Number(c3)
+
+c4 = Number(c4)
+
+console.log(c1.toFixed(5))
+
+console.log(c2.toFixed(5))
+
+console.log(c3.toFixed(5))
+
+console.log(c4.toFixed(5))
---
+++ HW-3/js/script4.js
@@ -0,0 +1,5 @@
+let randomNumbers;
+
+randomNumbers = Math.random(Math.random(randomNumbers) * (3 - 19) + 3);
+
+console.log(Number.parseInt(randomNumbers))

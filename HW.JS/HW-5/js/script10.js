b36a17c2c83516bd5fe26b972cb9797c70eaaf78
fgtucv <oles.slava23@gmail.com>
2023-09-01 15:09:10 +0300

    f

---
+++ HW.JS/HW-5/js/script10.js
@@ -0,0 +1,13 @@
+const randomNumber = Math.floor(Math.random() * 5) + 1;
+
+if (random_number === 1){
+    console.log("1");
+} else if (randomNumber === 2){
+    console.log("2")
+} else if (randomNumber === 3){
+    console.log("3")
+} else if (randomNumber === 4){
+    console.log("4")
+} else{
+    console.log("5")
+}
